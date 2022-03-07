1d0f11f6788691043ad00e9d1172320d9742bb20
DnineO <yakadzunvano@mail.ru>
2022-03-07 17:16:27 +0900

    update architecture

---
+++ users/main_page.php
@@ -0,0 +1,11 @@
+<?php
+
+
+require_once dirname(__DIR__, 1) . "/back/query.php";
+require_once dirname(__DIR__, 1) . "/back/templates.php";
+
+//alert('Привет');
+
+render_page("Main-page", render_template_main_page());
+
+?>
---
+++ users/user_page.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once dirname(__DIR__, 1) . "/back/query.php";
+require_once dirname(__DIR__, 1) . "/back/templates.php";
+
+//TODO: сделать рендер страницы пользователя, сверстать страницу
+
+?>
+
+<h1>HELLO</h1>
+<!--работает-->
