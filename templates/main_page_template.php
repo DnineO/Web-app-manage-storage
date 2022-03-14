8ec8c0d715921bcccd1c6188cf0a94fe430133c2
DnineO <yakadzunvano@mail.ru>
2022-03-14 19:26:51 +0900

    upload user pages

--- templates/main_page_template.php
+++ templates/main_page_template.php
@@ -1,8 +1,13 @@
 
 <div class="dropdown">
+    <h1> Main page</h1>
     <tr>
         <th><input type="button" class="button" value="Добавление товара"></input> </th>
         <th><input type="button" class="button" value="Оформление чего-то"></input></th>
     </tr>
 
+</div>
+
+<div>
+    список товаров мб ещё что то (додумать)
 </div>
---
+++ templates/user_page_template.php
@@ -0,0 +1,8 @@
+<div>
+    <h1> User profile page </h1>
+    <h1>
+        <?$surname = $_GET['surname'];
+        echo $surname?>
+
+    </h1>
+</div>
