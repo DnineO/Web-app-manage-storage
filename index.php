0c98d399f490093808fc0518c80f2d5eca0729c3
DnineO <yakadzunvano@mail.ru>
2022-03-14 18:51:37 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
+require_once dirname(__DIR__, 1)."/htdocs/back/functions.php";
 require_once dirname(__DIR__, 1)."/htdocs/back/query.php";
 require_once dirname(__DIR__, 1)."/htdocs/back/templates.php";
+
 
 render_page("Starter-page", render_template_index_form());
 
