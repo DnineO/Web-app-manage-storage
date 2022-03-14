450f711263f0174589bc5c1af2e08226e40d76db
DnineO <yakadzunvano@mail.ru>
2022-03-14 19:02:17 +0900

    Create functions.php

---
+++ back/functions.php
@@ -0,0 +1,22 @@
+<?php
+
+//проверяет авторизован ли пользователь
+function showPageIfLogged($title,$content){
+    if (session_status() == PHP_SESSION_NONE){
+        session_start();
+    }
+    if (isset($_SESSION['name'])){
+        render_page($title,$content);
+    }
+    else
+    {
+        render_page($title,"Вы не авторизованы");
+    }
+}
+
+//alert
+function alert($msg) {
+    echo "<script type='text/javascript'>alert('$msg');</script>";
+}
+
+?>
