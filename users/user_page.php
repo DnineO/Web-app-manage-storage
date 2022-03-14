e2c143b9b2269a8f93bb1b967ff4a9b415696be3
DnineO <yakadzunvano@mail.ru>
2022-03-14 19:35:49 +0900

    Update user_page.php

--- users/user_page.php
+++ users/user_page.php
@@ -4,7 +4,7 @@ require_once dirname(__DIR__, 1)."/back/functions.php";
 require_once dirname(__DIR__, 1) . "/back/query.php";
 require_once dirname(__DIR__, 1) . "/back/templates.php";
 
-//TODO: сделать рендер страницы пользователя, сверстать страницу
+//TODO: сделать рендер страницы пользователя(готово), сверстать страницу
 showPageIfLogged('User page',render_template_user_page());
 ?>
 
