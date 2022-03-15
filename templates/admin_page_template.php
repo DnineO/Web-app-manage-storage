276eaeb04efdaaa4f1a066d9e84a88a061dda030
DnineO <yakadzunvano@mail.ru>
2022-03-15 15:53:37 +0900

    update user,admin page

--- templates/admin_page_template.php
+++ templates/admin_page_template.php
@@ -12,3 +12,33 @@ $surname = $_SESSION['name'];
 <h3>
     Admin profile page - <?=$surname?>
 </h3>
+
+<div class="formData">
+    <table class="table">
+        <tbody>
+        <th>
+            <tr><td><span class="lead">Фамилия </span>
+                    <font size="4" color="#808080">(Иванов)</font></td>
+                <td><input class="form-check-input" name="text" value="<?=$surname;?>" disabled style="width:90%;"></td>
+            </tr>
+
+            <tr><td><span class="lead">Имя </span>
+                    <font size="4" color="#808080">(Иван)</font></td>
+                <td><input class="form-check-input" name="text" value="<?=$firstname;?>" disabled style="width:90%;"></td>
+            </tr>
+
+            <tr><td><span class="lead">Дата рождения </span>
+                    <font size="4" color="#808080"></font></td>
+                <td><input class="form-check-input" type="date" name="text" value="<?=$date_birth;?>" disabled style="width:90%;"></td>
+            </tr>
+
+            <tr><td><span class="lead">Роль</span>
+                    <font size="4" color="#808080"></font></td>
+                <td><input class="form-check-input" name="text" value="<?=$role_personal?>" disabled style="width:90%;"></td>
+            </tr>
+        </th>
+        </tbody>
+    </table>
+
+
+</div>
--- templates/user_page_template.php
+++ templates/user_page_template.php
@@ -1,8 +1,42 @@
-<div>
-    <h1> User profile page </h1>
-    <h1>
-        <?$surname = $_GET['surname'];
-        echo $surname?>
+<?php
+if (session_status() == PHP_SESSION_NONE){
+    session_start();
+}
+$surname = $_SESSION['name'];
+?>
 
-    </h1>
+
+<div>
+    <h1> User profile page <?=$surname?></h1>
+
+</div>
+<!--TODO: передачу данных, функционально можно разместить здесь кнопки, подумать-->
+<div class="formData">
+    <table class="table">
+        <tbody>
+        <th>
+            <tr><td><span class="lead">Фамилия </span>
+                    <font size="4" color="#808080">(Иванов)</font></td>
+                <td><input class="form-check-input" name="text" value="<?=$surname;?>" disabled style="width:90%;"></td>
+            </tr>
+
+            <tr><td><span class="lead">Имя </span>
+                    <font size="4" color="#808080">(Иван)</font></td>
+                <td><input class="form-check-input" name="text" value="<?=$firstname;?>" disabled style="width:90%;"></td>
+            </tr>
+
+            <tr><td><span class="lead">Дата рождения </span>
+                    <font size="4" color="#808080"></font></td>
+                <td><input class="form-check-input" type="date" name="text" value="<?=$date_birth;?>" disabled style="width:90%;"></td>
+            </tr>
+
+            <tr><td><span class="lead">Роль</span>
+                    <font size="4" color="#808080"></font></td>
+                <td><input class="form-check-input" name="text" value="<?=$role_personal?>" disabled style="width:90%;"></td>
+            </tr>
+        </th>
+        </tbody>
+    </table>
+
+
 </div>
