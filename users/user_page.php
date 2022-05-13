fd949559fb1701c7bde0acbfbdcacea2c87a0a91
DnineO <yakadzunvano@mail.ru>
2022-05-13 22:49:56 +0900

    Update user_page.php

--- users/user_page.php
+++ users/user_page.php
@@ -13,12 +13,7 @@ require_once dirname(__DIR__, 1) . "/back/templates.php";
 if (isset($_SESSION['name'])) {
 
     $user = get_admin($_SESSION['name']);
-//    print_r($user);
 
-    $surname = $user[0]['surname'];
-    $firstname = $user[0]['firstname'];
-    $date_birth = $user[0]['date_birthday'];
-    $role_personal = $user[0]['role_personal'];
     showPageIfLogged('User page',render_template_user_page($user[0]));
 }
 ?>
