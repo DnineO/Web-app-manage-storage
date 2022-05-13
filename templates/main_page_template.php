b35d6ac947dcc307570f40e72fb2c3716449b246
DnineO <yakadzunvano@mail.ru>
2022-05-13 22:49:51 +0900

    update pages

--- templates/main_page_template.php
+++ templates/main_page_template.php
@@ -1,52 +1,49 @@
 
 <div class="dropdown">
-<!--    <h1> Main page</h1>-->
-    <tr>
-        <th><input type="button" class="button" value="Оформление товара/услуги" onclick=""></th>
-        <th><input type="button" class="button" value="Отчет ИП"></th>
-        <th><input type="button" class="button" value="Прием/Отгрузка"></th>
-        <th><input type="button" class="button" value="Резервирование"></th>
-        <th><input type="button" class="button" value="Инвентаризация"></th>
-        <th><input type="button" class="button" value="Счет ИП"></th>
-    </tr>
-
+<!--    <tr>-->
+        <input type="button" class="btn btn-secondary" value="Оформление товара/услуги" onclick="">
+        <input type="button" class="btn btn-secondary" value="Отчет ИП">
+        <input type="button" class="btn btn-secondary" value="Прием/Отгрузка">
+        <input type="button" class="btn btn-secondary" value="Резервирование">
+        <input type="button" class="btn btn-secondary" value="Инвентаризация">
+        <input type="button" class="btn btn-secondary" value="Счет ИП">
+<!--    </tr>-->
 </div>
 
 <br>
 
-<div>
-    <table border="0" cellspacing="2"  cellpadding="5" width="90%">
-        <caption align="top" valign="top"> таблица товаров </caption>
-        <tr>
-            <th> № </th>
-            <th>Наименование</th>
-            <th>Форма</th>
-            <th></th>
-            <th></th>
-            <th>Страница</th>
-        </tr>
-        <tr><td>1</td><td>Адаптеры</td><td>Круглый</td><td>С врезкой</td><td>круглой</td><td><a href=""> перейти</a></td></tr>
-        <tr><td>2</td><td>Адаптеры</td><td>Круглый</td><td>С врезкой</td><td>прямоугольной</td><td><a href=""> перейти</a></td></tr>
-        <tr><td>3</td><td>Адаптеры</td><td>Круглый</td><td>С отверстием</td><td></td><td><a href=""> перейти</a></td></tr>
-        <tr><td>4</td><td>Адаптеры</td><td>Прямоугольный</td><td>С врезкой</td><td>круглой</td><td><a href=""> перейти</a></td></tr>
-        <tr><td>5</td><td>Адаптеры</td><td>Прямоугольный</td><td>С врезкой</td><td>прямоугольной</td><td><a href=""> перейти</a></td></tr>
-        <tr><td>6</td><td>Адаптеры</td><td>Прямоугольный</td><td>С отверстием</td><td></td><td><a href=""> перейти</a></td></tr>
-        <tr><td>7</td><td>Воздуховоды</td><td>Круглые</td><td></td><td></td><td><a href=""> перейти</a></td></tr>
-        <tr><td>8</td><td>Воздуховоды</td><td>Прямоугольные</td><td></td><td></td><td><a href=""> перейти</a></td></tr>
-        <tr><td>9</td><td>Врезки</td><td>Круглая</td><td>в диаметр</td><td></td><td><a href=""> перейти</a></td></tr>
-        <tr><td>10</td><td>Врезки</td><td>Круглая</td><td>прямая</td><td></td><td><a href=""> перейти</a></td></tr>
-        <tr><td>11</td><td>Врезки</td><td>Прямоугольная</td><td>в диаметр</td><td></td><td><a href=""> перейти</a></td></tr>
-        <tr><td>12</td><td>Врезки</td><td>Прямоугольная</td><td>прямая</td><td></td><td><a href=""> перейти</a></td></tr>
+<div class="btn-toolbar" >
+    <p>
+        <a class="btn btn-secondary" data-bs-toggle="collapse" href="#collapse1" role="button" aria-expanded="false" aria-controls="collapseExample">
+            Показать таблицу товаров
+        </a>
+    </p>
 
-    </table>
-</div>
-    <div class="item-list" align="center">
-        <ul>
-            <th><span>1</span></th>
-            <th><a href="" title="На страницу номер 2">2</a></th>
-            <th><a href="" title="На страницу номер 3">3</a></th>
-            <th><a href="" title="На страницу номер 4">4</a></th>
-            <th><a href="" title="На следующую страницу">›</a></th>
-        </ul>
+    <div class="collapse" id="collapse1">
+        <div class="card card-body">
+            render table product.
+            <?php
+            //TODO: таблицы
+            ?>
+        </div>
     </div>
+</div>
+
 <!--    список товаров мб ещё что то (додумать)-->
+
+<div class="btn-toolbar" >
+    <p>
+        <a class="btn btn-secondary" data-bs-toggle="collapse" href="#collapse2" role="button" aria-expanded="false" aria-controls="collapseExample">
+            Показать таблицу номенклатуры
+        </a>
+    </p>
+
+    <div class="collapse" id="collapse2">
+        <div class="card card-body">
+            render table waybill.
+            <?php
+
+            ?>
+        </div>
+    </div>
+</div>
