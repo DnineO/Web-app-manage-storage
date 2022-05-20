ee370ad9983996e3628d299af525a56a60630d1a
DnineO <yakadzunvano@mail.ru>
2022-05-21 01:13:24 +0900

    Create inventory_page.php
    new page

---
+++ users/inventory_page.php
@@ -0,0 +1,16 @@
+<?php
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+
+require_once dirname(__DIR__, 1)."/back/functions.php";
+require_once dirname(__DIR__, 1) . "/back/query.php";
+require_once dirname(__DIR__, 1) . "/back/templates.php";
+
+showPageIfLogged("Inventory page", render_template_inventory_page());
+
+
+?>
+
