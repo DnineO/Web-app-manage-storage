821a719831c781f672243c9440ca51b0efdce35b
DnineO <yakadzunvano@mail.ru>
2022-05-21 01:17:41 +0900

    Update reception_page_template.php
    update reception page template

--- templates/reception_page_template.php
+++ templates/reception_page_template.php
@@ -8,27 +8,20 @@
 <div>
     <h1>Оформление</h1>
     <form name="addproduct" action="" method="post">
-        <table class="tab-content">
-            <th>
-                <tr>
-                        <div class="input-group mb-3" style="width: 25%">
-                            <label class="input-group-text" for="select1">Операция</label>
-                            <select name="select1[]" class="form-select" id="select2">
-                                <option selected disabled>Выберите</option>
-                                <option value="Приход">Приход</option>
-                                <option value="Отгрузка">Отгрузка</option>
-                            </select>
-                        </div>
-                </tr>
-                <tr>
-                        <div class="btn-toolbar">
-                            <div>
-                                <p>
-                                    <button class="btn btn-secondary" type="submit">Продолжить</button>
-                                </p>
-                            </div>
-                </tr>
-            </th>
-        </table>
+        <div class="input-group mb-3" style="width: 25%">
+            <label class="input-group-text" for="select1">Операция</label>
+            <select name="select1[]" class="form-select" id="select2">
+                <option selected disabled>Выберите</option>
+                <option value="Приход">Приход</option>
+                <option value="Отгрузка">Отгрузка</option>
+            </select>
+        </div>
+        <div class="btn-toolbar">
+            <div>
+                <p>
+                    <button class="btn btn-secondary" type="submit">Продолжить</button>
+                </p>
+            </div>
+        </div>
     </form>
 </div>
