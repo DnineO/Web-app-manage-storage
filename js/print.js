14af095d153e91aa066d4719fd1a80c8521a781f
DnineO <yakadzunvano@mail.ru>
2022-05-20 21:46:40 +0900

    Update print.js
    doesn't work

--- js/print.js
+++ js/print.js
@@ -1,14 +1,13 @@
 
 // Вывод страницы на печать (пример)
-function callPrint() {
-    var printCSS = '<link rel="stylesheet" href="css/starter-template.css" type="text/css" /> ' +
-                    '<link href="../css/bootstrap.css" rel="stylesheet">';
-    var printTitle = document.getElementById('print-title').innerHTML;
-    var printText = document.getElementById('print-text').innerHTML;
+function callPrint(strid) {
+    var printContent = document.getElementById(strid)
+    var printCSS = '<link href="../css/bootstrap.css" rel="stylesheet">';
     var windowPrint = window.open('','','left=50,top=50,width=800,height=640,toolbar=0,scrollbars=1,status=0');
+    // windowPrint.document.write('<div id="print" class="contentpane">');
     windowPrint.document.write(printCSS);
-    windowPrint.document.write(printTitle);
-    windowPrint.document.write(printText);
+    windowPrint.document.write(printContent.innerHTML);
+    // windowPrint.document.write('</div>')
     windowPrint.document.close();
     windowPrint.focus();
     windowPrint.print();
