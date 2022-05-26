7c37f6a9dc3b17d9d332a70c1d7aeb39fc4c698c
DnineO <yakadzunvano@mail.ru>
2022-05-26 20:45:17 +0900

    Update adapter_circle_template.php

--- templates/products/adapter_circle_template.php
+++ templates/products/adapter_circle_template.php
@@ -10,31 +10,31 @@
                     <td>
                         <div class="input-group input-group-sm mb-3">
                             <span class="input-group-text" for="inputGroup-sizing-sm">D,мм</span>
-                            <input  type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                            <input name="D"  type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
                         </div>
                     </td>
                     <td>
                         <div class="input-group input-group-sm mb-3">
                             <span class="input-group-text" id="inputGroup-sizing-sm">A,мм</span>
-                            <input  type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                            <input name="A"  type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
                         </div>
                     </td>
                     <td>
                         <div class="input-group input-group-sm mb-3">
                             <span class="input-group-text" id="inputGroup-sizing-sm">B,мм</span>
-                            <input  type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                            <input name="B"  type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
                         </div>
                     </td>
                     <td>
                         <div class="input-group input-group-sm mb-3">
                             <span class="input-group-text" id="inputGroup-sizing-sm">L,мм</span>
-                            <input  type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                            <input name="L"  type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
                         </div>
                     </td>
                     <td>
                         <div class="input-group input-group-sm mb-3">
                             <span class="input-group-text" id="inputGroup-sizing-sm">Кол-во</span>
-                            <input  type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                            <input name="count_order"  type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
                         </div>
                     </td>
                     <td>
