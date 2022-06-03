8db46420d6b87adeb22aa13a62a9a93f8cf85ac2
DnineO <yakadzunvano@mail.ru>
2022-06-03 20:59:21 +0900

    update pages

--- admins/product_page.php
+++ admins/product_page.php
@@ -11,18 +11,7 @@ require_once dirname(__DIR__, 1) . "/back/templates.php";
 
 //showPageIfLogged("Product-page", render_template_product_page());
 
-if (isset($_POST['count']) or isset($_POST['price'])){
-//    var_dump($_POST);
+showPageIfLogged("Product-page", render_template_product_admin_page());
 
-    $id_product = $_POST['id_product'];
-    $counter = $_POST['count'];
-    $price = $_POST['price'];
-
-    update_product($id_product,$counter,$price);
-    showPageIfLogged("Product-page", render_template_product_page());
-    alert('Сохранено успешно');
-}else{
-    showPageIfLogged("Product-page", render_template_product_page());
-}
 
 ?>
--- admins/users_admin_page.php
+++ admins/users_admin_page.php
@@ -8,6 +8,23 @@ require_once dirname(__DIR__, 1)."/back/query.php";
 require_once dirname(__DIR__, 1)."/back/templates.php";
 require_once dirname(__DIR__, 1)."/back/functions.php";
 
+//var_dump($_POST);
 
+if (isset($_POST['delete'])){
+    delete_agent($_POST['id']);
+    alert('Пользователь №'.$_POST['id'].' удален');
+}
+
+if (isset($_POST['add_user'])){
+    $surname = $_POST['surname'];
+    $firstname = $_POST['firstname'];
+    $date = $_POST['date_birth'];
+    $role = $_POST['role'][0];
+    $pass = $_POST['pass'];
+    push_agent($surname,$firstname,$date,$role,$pass);
+    alert('Добавление успешно');
+}
 
 showPageIfLogged("Users page",render_template_users_admin_page());
+
+?>
